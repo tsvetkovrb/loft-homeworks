45b494d56c68350b244e9ac74f789c3aafcd17cc
Roman Tsvetkov <tsvetkovv22@gmail.com>
2019-11-15 22:52:03 +0300

    Checking codestyle

--- webpack.config.rules.js
+++ webpack.config.rules.js
@@ -1,20 +1,20 @@
 module.exports = [
-    {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: { cacheDirectory: true }
+  {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    loader: 'babel-loader',
+    options: { cacheDirectory: true },
+  },
+  {
+    test: /\.hbs/,
+    loader: 'handlebars-loader',
+  },
+  {
+    test: /\.(jpe?g|png|gif|svg|eot|ttf|woff|woff2)$/i,
+    loader: 'file-loader',
+    options: {
+      name: '[hash:8].[ext]',
+      outputPath: 'reosurces',
     },
-    {
-        test: /\.hbs/,
-        loader: 'handlebars-loader'
-    },
-    {
-        test: /\.(jpe?g|png|gif|svg|eot|ttf|woff|woff2)$/i,
-        loader: 'file-loader',
-        options: {
-            name: '[hash:8].[ext]',
-            outputPath: 'reosurces'
-        }
-    }
+  },
 ];
