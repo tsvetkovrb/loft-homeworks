a7bd8c724028c8a2fbd8f1411f9c93cd231d2110
roman.tsvetkov <rtcvetkov@aicorner.org>
2019-12-16 11:54:55 +0300

    Refactoring

--- src/index.js
+++ src/index.js
@@ -1,45 +1,18 @@
-import { compile } from 'handlebars'
+import { compile } from 'handlebars';
+
+import placemarkGrey from './static/placemarkGrey.svg';
+import circleOrange from './static/circleOrange.svg';
 
 import './main.scss';
-import placemarkGrey from './static/placemarkGrey.svg'
-import circleOrange from './static/circleOrange.svg'
 
 const template = document.getElementById('balloon-template').textContent;
 const render = compile(template);
 
 const reviewsMap = new Map();
 
-class MarkStore {
-  constructor() {
-    this.markList = []
-  }
-
-  add(mark) {
-    this.markList.push(mark)
-    try {
-      localStorage.setItem('placemark', JSON.stringify(this.markList))
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  get() {
-    try {
-      const placemarks = localStorage.getItem('placemark')
-
-      return JSON.parse(placemarks)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-}
-
-const markStore = new MarkStore();
-
 ymaps.ready(init);
 
 function init() {
-  // Создание карты.
   let map = new ymaps.Map('map', {
     center: [59.93545698, 30.32687934],
     zoom: 16,
@@ -49,18 +22,16 @@ function init() {
   const MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
     '<div style="color: #000000; font-weight: bold;">{{ properties.geoObjects.length }}</div>',
   );
-  // Создаем собственный макет с информацией о выбранном геообъекте.
-  var customItemContentLayout = ymaps.templateLayoutFactory.createClass(
+  const customItemContentLayout = ymaps.templateLayoutFactory.createClass(
     `
       <div class="balloon__place">{{ properties.balloonContentPlace }}</div>
-      <a href="" class="balloon__address">{{ properties.balloonContentAddress }}</a>
+      <div class="balloon__address">{{ properties.balloonContentAddress }}</div>
       <div class="balloon__message">{{ properties.balloonContentMessage }}</div>
       <div class="balloon__datetime">{{ properties.balloonContentDatetime }}</div>
     `,
     {
       build: function() {
         customItemContentLayout.superclass.build.call(this);
-        // А затем выполняем дополнительные действия.
         this.linkBalloonButton = document.querySelector('.balloon__address');
         this.linkBalloonListener = this.linkBalloonListener.bind(this);
         this.linkBalloonButton.addEventListener(
@@ -102,16 +73,11 @@ function init() {
       },
     ],
     clusterIconContentLayout: MyIconContentLayout,
-    // Устанавливаем стандартный макет балуна кластера "Карусель".
     clusterBalloonContentLayout: 'cluster#balloonCarousel',
-    // Устанавливаем собственный макет.
     clusterBalloonItemContentLayout: customItemContentLayout,
-    // В данном примере балун никогда не будет открываться в режиме панели.
     clusterBalloonPanelMaxMapArea: 0,
-    // Устанавливаем размеры макета контента балуна (в пикселях).
     clusterBalloonContentLayoutWidth: 300,
     clusterBalloonContentLayoutHeight: 200,
-    // Устанавливаем максимальное количество элементов в нижней панели на одной странице
     clusterBalloonPagerSize: 5,
     disableClickZoom: true,
     hideIconOnBalloonOpen: false,
@@ -127,13 +93,10 @@ function init() {
     }
     const html = render({ cond: reviews.length === 0, address, reviews });
     const BalloonContentLayout = ymaps.templateLayoutFactory.createClass(html, {
-
       build: function() {
-        // Сначала вызываем метод build родительского класса.
         BalloonContentLayout.superclass.build.call(this);
-        // А затем выполняем дополнительные действия.
-        this._$element = document.querySelector('.geo');
-        this.closeBalloonButton = document.querySelector('.geo__close');
+        this._$element = document.querySelector('.balloon');
+        this.closeBalloonButton = document.querySelector('.balloon__close');
         this.form = document.querySelector('.review');
         this.submitForm = document.querySelector('.review__button');
         this.addReviewListener = this.addReviewListener.bind(this);
@@ -144,8 +107,6 @@ function init() {
         );
       },
       clear: function() {
-        // Выполняем действия в обратном порядке - сначала снимаем слушателя,
-        // а потом вызываем метод clear родительского класса.
         this.closeBalloonButton.removeEventListener(
           'click',
           this.closeBalloonListener,
@@ -174,7 +135,7 @@ function init() {
           parseInt(currentMonth + 1) < 10
             ? `0${parseInt(currentMonth) + 1}`
             : parseInt(currentMonth) + 1;
-        
+
         return `${month}/${day}/${newDate.getFullYear()}`;
       },
       addReviewListener: function(e) {
@@ -192,11 +153,9 @@ function init() {
         } else {
           reviews = [review];
         }
-        // обновляем данные в Map
         reviewsMap.set(address, reviews);
-        // обновляем балун, делаем перерендер
         this._renderedTemplate.text = render({
-          cond: reviews.length === 0,
+          noReview: reviews.length === 0,
           address,
           reviews,
         });
@@ -209,20 +168,14 @@ function init() {
           balloonContentAddress: address,
           balloonContentMessage: review.message,
           balloonContentDatetime: review.date,
-        }
-        
-        markStore.add(mark)
+        };
 
-        let placemark = new ymaps.Placemark(
-          coords,
-          mark,
-          {
-            iconLayout: 'default#image',
-            iconImageHref: placemarkGrey,
-            iconImageSize: [44, 66],
-            iconImageOffset: [-22, -66],
-          },
-        );
+        const placemark = new ymaps.Placemark(coords, mark, {
+          iconLayout: 'default#image',
+          iconImageHref: placemarkGrey,
+          iconImageSize: [44, 66],
+          iconImageOffset: [-22, -66],
+        });
 
         placemark.events.add('click', function() {
           const BalloonContentLayout = renderBalloon(coords, address);
@@ -234,13 +187,12 @@ function init() {
         clusterer.add(placemark);
       },
     });
-    
+
     return BalloonContentLayout;
   }
 
   map.events.add('click', async e => {
     try {
-      // Получение координат щелчка
       const coords = e.get('coords');
       const geocoder = await ymaps.geocode(coords);
       const firstGeoObject = geocoder.geoObjects.get(0);
@@ -254,19 +206,4 @@ function init() {
       console.log(error);
     }
   });
-
-  const placemarks = markStore.get()
-
-  if (placemarks.length > 0) {
-    for (const mark of placemarks) {
-      const place = new ymaps.Placemark(mark.balloonContentCoords, mark, {
-        iconLayout: 'default#image',
-        iconImageHref: placemarkGrey,
-        iconImageSize: [44, 66],
-        iconImageOffset: [-22, -66],
-      })
-
-      clusterer.add(place);
-    }
-  }
 }
